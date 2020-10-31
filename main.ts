c8f67dbb9ef87006cd2ae87ce55b56a934dd746a
Lemonexus <67396147+Lemonexus@users.noreply.github.com>
2020-10-31 13:52:30 -0400

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,72 +1,110 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (ashley.isHittingTile(CollisionDirection.Bottom) || ashley.isHittingTile(CollisionDirection.Right) || ashley.isHittingTile(CollisionDirection.Left)) {
+        ashley.vy = -250
+    }
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    ashley.setImage(leftFacingImg)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    ashley.setImage(rightFacingImg)
+})
+let ashley: Sprite = null
+let leftFacingImg: Image = null
+let rightFacingImg: Image = null
+rightFacingImg = img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . f f f f f f f . . . 
+    . . . . f f f f e e e e e f . . 
+    . . . f e e e e e e e e e f . . 
+    . . f e e e e e e e e e e f . . 
+    . . f e e e e e e e e e e e f . 
+    . . f e e e e e e e e e e e f . 
+    . . f e e e e f f e e e e e f . 
+    . f f e e e e f b f e e e f . . 
+    . f e e e e e e 1 f 4 4 e . . . 
+    . f e e e e e f 4 4 4 4 f . . . 
+    . . f e e e d d d d d d f . . . 
+    . . . f f f 3 8 2 d f f f . . . 
+    . . . f d d d d d d d d d f . . 
+    . . . . f f f d d d d f f f . . 
+    . . . . . f f f f f f f f . . . 
+    `
+leftFacingImg = img`
+    . . . f e e f f f f . . . . . . 
+    . . f e e e e e f e f f . . . . 
+    . . f e e e e e e e e e f . . . 
+    . . f e e e e e e e e e e f . . 
+    . . f e e e e e e e e e e f . . 
+    . f e e e e e e e e e e e f . . 
+    . f e e e e e f f e e e e f . . 
+    . f e e e e f b f e e e e f . . 
+    . . f e 4 4 f 1 e e e e e f . . 
+    . . . f 4 4 4 4 f e e e e f f . 
+    . . . f 4 4 4 f f f e e e e f . 
+    . . . f d d d d d d f e e f . . 
+    . . . f 2 3 9 d f f f f f . . . 
+    . . f d d d d d d d d f . . . . 
+    . . f f d d d d d d f f . . . . 
+    . . . . f f f f f f . . . . . . 
+    `
 let rightSwordOutImg = img`
-    . . . . . . . f f . . . . . . . 
-    . . . . f f f f 2 f f . . . . . 
-    . . f f e e e e f 2 f f . . . . 
-    . f f e e e e e f 2 2 f f . . . 
-    . f e e e e f f e e e e f . . . 
-    . f f f f f e e 2 2 2 2 e f . . 
-    f f f e 2 2 2 f f f f e 2 f . . 
-    f f f f f f f f e e e f f f . . 
-    f e f e 4 4 e b f 4 4 e e f . . 
-    . f e e 4 d 4 b f d d e f . . . 
-    . . f e e e 4 d d d e e . c . . 
-    . . . f 2 2 2 2 e e d d e c c c 
-    . . . f 4 4 4 e 4 4 d d e c d d 
-    . . . f f f f f e e e e . c c c 
-    . . f f f f f f f f . . . c . . 
-    . . f f f . . f f . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . f f f f f f f . . . 
+    . . . . f f f f e e e e e f . . 
+    . . . f e e e e e e e e e f . . 
+    . . f e e e e e e e e e e f . . 
+    . . f e e e e e e e e e e e f . 
+    . . f e e e e e e e e e e e f . 
+    . . f e e e e f f e e e e e f . 
+    . f f e e e e f b f e e e f . . 
+    . f e e e e e e 1 f 4 4 e . . f 
+    . f e e e e e f 4 4 4 4 f f . f 
+    . . f e e e d d d d d d f 4 f 2 
+    . . . f f f 2 3 8 d f f f 4 f 2 
+    . . . f d d d d d d d d d f . f 
+    . . . . f d d d d d d d d f . f 
+    . . . . . f f f d d f f f . . . 
     `
 let leftSwordOutImg = img`
-    . . . . . . . f f . . . . . . . 
-    . . . . . f f 2 f f f f . . . . 
-    . . . . f f 2 f e e e e f f . . 
-    . . . f f 2 2 f e e e e e f f . 
-    . . . f e e e e f f e e e e f . 
-    . . f e 2 2 2 2 e e f f f f f . 
-    . . f 2 e f f f f 2 2 2 e f f f 
-    . . f f f e e e f f f f f f f f 
-    . . f e e 4 4 f b e 4 4 e f e f 
-    . . . f e d d f b 4 d 4 e e f . 
-    . . c . e e d d d 4 e e e f . . 
-    c c c e d d e e 2 2 2 2 f . . . 
-    d d c e d d 4 4 e 4 4 4 f . . . 
-    c c c . e e e e f f f f f . . . 
-    . . c . . . f f f f f f f f . . 
-    . . . . . . . f f . . f f f . . 
-    `
-let rightFacingImg = img`
     . . . . . . . . . . . . . . . . 
-    . . . . . f f f f f f . . . . . 
-    . . . f f e e e e f 2 f . . . . 
-    . . f f e e e e f 2 2 2 f . . . 
-    . . f e e e f f e e e e f . . . 
-    . . f f f f e e 2 2 2 2 e f . . 
-    . . f e 2 2 2 f f f f e 2 f . . 
-    . f f f f f f f e e e f f f . . 
-    . f f e 4 4 e b f 4 4 e e f . . 
-    . f e e 4 d 4 1 f d d e f . . . 
-    . . f e e e e e d d d f . . . . 
-    . . . . f 4 d d e 4 e f . . . . 
-    . . . . f e d d e 2 2 f . . . . 
-    . . . f f f e e f 5 5 f f . . . 
-    . . . f f f f f f f f f f . . . 
-    . . . . f f . . . f f f . . . . 
+    . . . f f f f f f f . . . . . . 
+    . . f e e e e e f e f f . . . . 
+    . . f e e e e e e e e e f . . . 
+    . . f e e e e e e e e e e f . . 
+    . f e e e e e e e e e e e f . . 
+    . f e e e e e e e e e e e f . . 
+    . f e e e e e f f e e e e f . . 
+    . . f e e e f b f e e e e f f . 
+    f . . e 4 4 f 1 e e e e e e f . 
+    f . f f 4 4 4 4 f e e e e e f . 
+    2 f 4 f d d d d d f e e e f . . 
+    2 f 4 f 2 3 d f f f f f f . . . 
+    f . f d d d d d d d d d f . . . 
+    f . f d d d d d d d d f . . . . 
+    . . . f f f d d f f f . . . . . 
     `
-let leftFacingImg = img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . f f f f f f . . . . . 
-    . . . . f 2 f e e e e f f . . . 
-    . . . f 2 2 2 f e e e e f f . . 
-    . . . f e e e e f f e e e f . . 
-    . . f e 2 2 2 2 e e f f f f . . 
-    . . f 2 e f f f f 2 2 2 e f . . 
-    . . f f f e e e f f f f f f f . 
-    . . f e e 4 4 f b e 4 4 e f f . 
-    . . . f e d d f 1 4 d 4 e e f . 
-    . . . . f d d d e e e e e f . . 
-    . . . . f e 4 e d d 4 f . . . . 
-    . . . . f 2 2 e d d e f . . . . 
-    . . . f f 5 5 f e e f f f . . . 
-    . . . f f f f f f f f f f . . . 
-    . . . . f f f . . . f f . . . . 
-    `
+ashley = sprites.create(rightFacingImg, SpriteKind.Player)
+controller.moveSprite(ashley, 100, 0)
+ashley.ay = 350
+scene.cameraFollowSprite(ashley)
+tiles.setTilemap(tilemap`level`)
+tiles.placeOnTile(ashley, tiles.getTileLocation(4, 30))
+game.onUpdate(function () {
+    if (ashley.tileKindAt(TileDirection.Right, sprites.dungeon.collectibleRedCrystal)) {
+        game.over(true)
+    }
+})
+game.onUpdateInterval(100, function () {
+    if (ashley.isHittingTile(CollisionDirection.Right) && ashley.vy > 0) {
+        ashley.ay = 0
+        ashley.vy = 15
+        ashley.setImage(rightSwordOutImg)
+    } else if (ashley.isHittingTile(CollisionDirection.Left) && ashley.vy > 0) {
+        ashley.ay = 0
+        ashley.vy = 15
+        ashley.setImage(leftSwordOutImg)
+    } else {
+        ashley.ay = 350
+    }
+})
